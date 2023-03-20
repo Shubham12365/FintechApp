f3eca1893ca43eaddf0b3aa37e2ea30cca2eae39
Manu Lingwal <lingwal908@gmail.com>
2023-03-20 15:36:57 +0530

    solving for india

---
+++ app/src/main/java/com/example/loginsignup/registerr.kt
@@ -0,0 +1,11 @@
+package com.example.loginsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registerr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registerr)
+    }
+}
